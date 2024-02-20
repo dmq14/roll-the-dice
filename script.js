9524365a41d4da5ccf5c57ed2c57d2b7bedcae75
dmq14 <dangminhquang1401@gmail.com>
2024-02-20 12:13:17 +0700

    update dice

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
-var elDiceOne       = document.getElementById('dice1');
-var elComeOut       = document.getElementById('roll');
+var elDiceOne = document.getElementById('dice1');
+var elComeOut = document.getElementById('roll');
+var elInfoForm = document.getElementById('infoForm');
+elComeOut.onclick = function () {
+  rollWeightedDice();
+  openModal()
+};
 
-elComeOut.onclick   = function () {rollDice();};
+function rollWeightedDice() {
+  var weightedResults = {
+    1: 0.2, //1
+    2: 0.2, //5
+    3: 0.2, //6
+    4: 0.2, //3
+    5: 0.2, //4
+    6: 0    //2
+  };
 
-function rollDice() {
-    var diceOne = getRandomNumber(1, 6);
-    //1 -> 1
-    //2 -> 5
-    //3 -> 6
-    //4 -> 3
-    //5 -> 4
-    //6 -> 2
-    for (var i = 1; i <= 6; i++) {
-      elDiceOne.classList.remove('show-' + i);
-      if (diceOne === i) {
-        elDiceOne.classList.add('show-' + i);
+  var totalWeight = Object.values(weightedResults).reduce((a, b) => a + b, 0);
+
+  var randomValue = Math.random() * totalWeight;
+  var cumulativeWeight = 0;
+  var diceOne;
+
+  for (var key in weightedResults) {
+    if (weightedResults.hasOwnProperty(key)) {
+      cumulativeWeight += weightedResults[key];
+      if (randomValue <= cumulativeWeight) {
+        diceOne = parseInt(key);
+        break;
       }
     }
-    console.log('Dice: ' + diceOne);
+  }
+
+  updateDiceDisplay(diceOne);
+  console.log('Dice: ' + diceOne);
 }
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+function updateDiceDisplay(result) {
+  for (var i = 1; i <= 6; i++) {
+    elDiceOne.classList.remove('show-' + i);
+    if (result === i) {
+      elDiceOne.classList.add('show-' + i);
+    }
+  }
 }
+function openModal() {
+    document.getElementById('infoModal').classList.remove('d-none');
+  }
+  
+  function closeModal() {
+    document.getElementById('infoModal').classList.add('d-none');
+  }
