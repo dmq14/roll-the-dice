23e6bec7aa38e58afb83175a4e3d0e96054d0cd8
dmq14 <dangminhquang1401@gmail.com>
2024-02-21 10:59:14 +0700

    update

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
 var elDiceOne = document.getElementById('dice1');
 var elComeOut = document.getElementById('roll');
 var elInfoForm = document.getElementById('infoForm');
+var diceOne;
+window.addEventListener('load', handlePageLoad);
+function handlePageLoad() {
+  var playedStatus = localStorage.getItem('status');
+  var getGiftElement = document.getElementById('get-coupon');
+    if (playedStatus === 'played') {
+      getGiftElement.style.display = 'block';
+
+    }else{
+      getGiftElement.style.display = 'none';
+    }
+}
 elComeOut.onclick = function () {
-  rollWeightedDice();
-  openModal()
+  var storedStatus = localStorage.getItem('status');
+  if (storedStatus && storedStatus === 'played') {
+    alert('Mỗi người chơi chỉ có 1 lượt thử vận may!!!')
+  }else{
+    rollWeightedDice();
+    setTimeout(function () {
+      getInfo();
+      handlePageLoad() 
+    }, 1500);
+  }
+
 };
 
 function rollWeightedDice() {
   var weightedResults = {
     1: 0.2, //1
     2: 0.2, //5
-    3: 0.2, //6
+    3: 0, //6
     4: 0.2, //3
     5: 0.2, //4
-    6: 0    //2
+    6: 0.2   //2
   };
 
   var totalWeight = Object.values(weightedResults).reduce((a, b) => a + b, 0);
 
   var randomValue = Math.random() * totalWeight;
   var cumulativeWeight = 0;
-  var diceOne;
+
 
   for (var key in weightedResults) {
     if (weightedResults.hasOwnProperty(key)) {
@@ -33,7 +54,6 @@ function rollWeightedDice() {
   }
 
   updateDiceDisplay(diceOne);
-  console.log('Dice: ' + diceOne);
 }
 
 function updateDiceDisplay(result) {
@@ -44,10 +64,44 @@ function updateDiceDisplay(result) {
     }
   }
 }
-function openModal() {
-    document.getElementById('infoModal').classList.remove('d-none');
-  }
-  
-  function closeModal() {
-    document.getElementById('infoModal').classList.add('d-none');
-  }
+function getInfo() {
+
+    var coupon = checkPoint(diceOne);
+    $('#modalCoupon').modal('show');
+    localStorage.setItem('status', 'played');
+    localStorage.setItem('dice', diceOne);
+    localStorage.setItem('coupon', coupon.code);
+    localStorage.setItem('description', coupon.description);
+
+}
+
+function checkPoint() {
+    var coupon = {
+        code: '',
+        description: ''
+    };
+    
+    if (diceOne == 1) {
+        coupon.code = 'SALE-Q100';
+        coupon.description = 'Coupon giá trị 100K';
+    } else if (diceOne == 6) {
+        coupon.code = 'SALE-E200';
+        coupon.description = 'Coupon giá trị 200K';
+    } else if (diceOne == 4) {
+        coupon.code = 'SALE-T300';
+        coupon.description = 'Coupon giá trị 300K';
+    } else if (diceOne == 5) {
+        coupon.code = 'SALE-Y400';
+        coupon.description = 'Coupon giá trị 400K';
+    } else if (diceOne == 2) {
+        coupon.code = 'SALE-P500';
+        coupon.description = 'Coupon giá trị 500K';
+    } else if (diceOne == 3) {
+        coupon.code = 'SALE-M600';
+        coupon.description = 'Coupon giá trị 600K';
+    }  else {
+        coupon.code = 'SALE-CD34';
+        coupon.description = 'Coupon giá trị 100K';
+    }
+    return coupon;
+}
